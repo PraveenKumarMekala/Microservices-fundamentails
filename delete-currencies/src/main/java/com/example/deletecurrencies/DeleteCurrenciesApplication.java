492f1485ebe7eab861a013840a3fc047c0bd85f7
PraveenKumarMekala <praveenkuruva@gmail.com>
2021-06-30 18:04:48 +0530

    Add files via upload

---
+++ delete-currencies/src/main/java/com/example/deletecurrencies/DeleteCurrenciesApplication.java
@@ -0,0 +1,13 @@
+package com.example.deletecurrencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteCurrenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleteCurrenciesApplication.class, args);
+	}
+
+}
---
+++ delete-currencies/src/test/java/com/example/deletecurrencies/DeleteCurrenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deletecurrencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleteCurrenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
